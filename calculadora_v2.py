dab9e1c2fa0cc9c641e2ea238dc1f82fa9197b9f
celsolemes <celsolemescs@gmail.com>
2025-03-03 20:07:15 -0300

    Update calculadora_v2.py

--- calculadora_v2.py
+++ calculadora_v2.py
@@ -1,24 +1,19 @@
-
-
-# Função de adição
+# Funções da calculadora
 def adicao(a, b):
     return a + b
 
-# Função de subtração
 def subtracao(a, b):
     return a - b
 
-# Função de multiplicação
 def multiplicacao(a, b):
     return a * b
 
-# Função de divisão
 def divisao(a, b):
     if b == 0:
         return "Não foi possível realizar a divisão por 0"
     return a / b
 
-# Função principal da calculadora
+# Calculadora
 def calculadora(num1, num2, operacao):
     if operacao in ('+', 'soma'):
         resultado = adicao(num1, num2)
@@ -33,7 +28,7 @@ def calculadora(num1, num2, operacao):
     
     return resultado
 
-# Loop para interagir com o usuário
+# Interação com o usuário
 while saida.lower() != 'n': 
     try:
         num1 = float(input("Digite o primeiro número: "))
