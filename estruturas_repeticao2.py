546f2a90aaa19d3003d61ed226ea42669c18f70e
celsolemes <celsolemescs@gmail.com>
2025-03-03 20:18:41 -0300

    Delete estruturas_repeticao2.py

--- estruturas_repeticao2.py
+++
@@ -1,9 +0,0 @@
-# Primeira parte: iterar sobre a string "Olá, laço for."
-texto = 'Olá, laço for.'
-
-for item in texto:
-    print('Caractere:', item)
-
-# Segunda parte: iterar sobre um intervalo de 1 a 10
-for numero in range(1, 11):  # range(1, 11) gera números de 1 a 10
-    print('Número do intervalo: ' + str(numero))  # Convertendo número para string
